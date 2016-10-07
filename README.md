7cbf367d85b6107ee3fc8a72d8b4049e57fbc8bb
kurbhatt <info.keyurbhatt@gmail.com>
2016-10-07 14:21:13 +0530

    Actions:     1. Added code for to demonstrate the lambda expression from Java 8.
    Commit: 005

--- README.md
+++ README.md
@@ -2,14 +2,15 @@
 
 This application perform following task:
   - File creation and insert data inside file.
-  - Convert Date to XMLGregorianCalendar
+  - Convert Date to XMLGregorianCalendar.
   - List declared methods and members of class using reflection.
   - Access private member's value without using getter setter.
   - Overload main method.
   - main method execution order.
   - Encryption and Decryption of string using AES algorithm using encryption key.
   - Load multiple properties file in single property instance.
-  - Sort HashMap by it's value
+  - Sort HashMap by it's value.
+  - Lambda Expression(Java 8) Demonstration. **(To Run/Execute this feature Java 8 is necessary)**
 
 For further Query or Found any Issue Please Contact:
 
---
+++ src/org/java8/LambdaExp.java
@@ -0,0 +1,47 @@
+package org.java8;
+
+/**
+ * Created by Keyur on 07-10-2016.
+ * This Class Demonstrate the functionality of Lambda Expression (Java 8).
+ */
+public class LambdaExp {
+
+	public static void main(String[] args) {
+		//Run this method will print below message to console.
+		jRunner.run();
+
+		Operation plus = (int a, int b) -> a + b;
+		Operation minus = (int a, int b) -> a - b;
+
+		//Implement giveMeValue and override it with assigning Pie Value
+		Value pieValue = () -> 3.14;
+
+		LambdaExp exp = new LambdaExp();
+
+		System.out.println("Plus Calculation : " + exp.calculate(10, 5, plus));
+		System.out.println("Minus Calculation : " + exp.calculate(10, 5, minus));
+		System.out.println("Pie Value : " + pieValue.giveMeValue());
+	}
+
+	// Create Runnable instance that will have a short implementation
+	private static Runnable jRunner = () -> {
+		System.out.println("Lambda Expression Called");
+	};
+
+
+	interface Operation {
+
+		int operation(int a, int b);
+	}
+
+
+	interface Value {
+
+		Object giveMeValue();
+	}
+
+	private int calculate(int a, int b, Operation mathOperation) {
+		return mathOperation.operation(a, b);
+	}
+
+}
